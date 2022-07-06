d3a02821e31ea409871bfbcef7b9e33381ab5a1a
Sonal Singh <sonal.singh.19993@gmail.com>
2022-07-06 16:09:18 +0700

    sm6250-common: Remove ANXCamera and build MiuiCamera
    Signed-off-by: nekoshirro <ais.muzakky@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -230,6 +230,3 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit proprietary blobs
 -include vendor/xiaomi/sm6250-common/BoardConfigVendor.mk
-
-# Inherit ANXCamera blobs
--include vendor/ANXCamera/BoardConfigAnx.mk
